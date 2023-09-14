4565909b12e296eea50dacb1fbfe2bace29628c2
Kaiming Cui <ckm@nao.cas.cn>
2023-09-14 15:00:00 +0530

    Add init file

--- src/deep_lc/__init__.py
+++ src/deep_lc/__init__.py
@@ -1,1 +1,17 @@
+# -*- coding: utf-8 -*-
+__author__ = "Kaiming Cui"
+__license__ = "MIT"
 __version__ = "0.1.0"
+
+from .classifier import DeepLC
+from .finetune import finetune
+from .utils import light_curve_preparation
+from .dataset import LocalDataset
+
+
+__all__ = [
+    "DeepLC",
+    "finetune",
+    "light_curve_preparation",
+    "LocalDataset",
+]
